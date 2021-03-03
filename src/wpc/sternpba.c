a6d73cdb13725644f1adda46bb131243a22d4b61
freezy <freezy@kodi.tv>
2021-03-04 00:13:37 +0100

    spa: Add improvements.

--- src/wpc/sternpba.c
+++ src/wpc/sternpba.c
@@ -12,7 +12,7 @@ Licensed under the original MAME license.  Other required change, in driver.c:
 DRIVER(spagb, 100)
 
 This code is quite messy, it is a quick one-off implementation.  It also contains many hacks applied to a specific
-version of SternGB.DLL to give us access to things the TPA interface does not and work around bugs.  
+version of SternGB.DLL to give us access to things the TPA interface does not and work around bugs.
 
 ************************************************************************************************/
 
@@ -21,9 +21,7 @@ version of SternGB.DLL to give us access to things the TPA interface does not an
 #include "sim.h"
 #include "vpintf.h"
 #include "video.h"
-#include "cpu/at91/at91.h"
 #include "sndbrd.h"
-#include "dmddevice.h"
 #include "mech.h"
 #include <stdint.h>
 #include "zlib.h"
@@ -31,40 +29,31 @@ version of SternGB.DLL to give us access to things the TPA interface does not an
 
 // Defines
 #define SPA_SOUNDFREQ 48000
-#define SPA_SNDBUFSIZE SPA_SOUNDFREQ * 70 / 1000
-#define GEN_SPA GEN_SAM
+#define SPA_SNDBUFSIZE (SPA_SOUNDFREQ * 70 / 1000)
 #define SPA_GAME_GB 1
+
+#define REFRESH_HACK
+
 //Static Variables
-static int time_to_quit;
-void uSleep(const UINT64 u);
+static char spa_dllpath[MAX_PATH];
+extern int time_to_quit;
 
 /*----------------
 / Local variables
 /-----------------*/
 struct {
 	int vblankCount;
-	int diagnosticLed;
-	int sw_stb;
-	int zc;
-	int video_page[2];
-	INT16 samplebuf[2][SPA_SNDBUFSIZE];
-	INT16 lastsamp[2];
+	INT16 samplebuf[SPA_SNDBUFSIZE];
+	INT16 lastsamp;
 	int sampout;
 	int sampnum;
-	UINT8 volume[2], mute[2], DAC_mute[2]; //0..127, 0/1
-	int pass;
 	int coindoor;
-	int samVersion; // 1 or 2
-	UINT16 value;
-	INT16 bank;
-	UINT8 miniDMDData[14][16];
-	UINT32 solenoidbits[CORE_MODSOL_MAX];
-	UINT32 modulated_lights[WOF_MINIDMD_MAX];  // Also used by Tron ramps
-	UINT8 modulated_lights_prev_levels[WOF_MINIDMD_MAX];
+#ifdef REFRESH_HACK
 	int refresh_size;
+#endif
 } spalocals;
 
-uint8_t *spa_fbuffer, *spa_bbuffer, *persistbuf;
+static uint8_t *spa_fbuffer, *spa_bbuffer, *persistbuf;
 
 int(*stern_set_rom_data_fn)(uint8_t *, int);
 int(*stern_set_persistant_data_fn)(void *, int);
@@ -112,26 +101,22 @@ FILE *fpSND = NULL;
 #endif
 
 
-#define MAKE16BIT(x,y) (((x)<<8)|(y))
-
-
-
 /*-------------------------
 / Machine driver constants
 /--------------------------*/
 /*-- Common Inports for SAM Games, could update for SPA --*/
 #define SPA_COMPORTS \
   PORT_START /* 0 */ \
-	/*Switch Col. 0*/ \
+    /*Switch Col. 0*/ \
     COREPORT_BITDEF(  0x0010, IPT_TILT,           KEYCODE_INSERT)  \
     COREPORT_BIT   (  0x0020, "Slam Tilt",        KEYCODE_HOME)  \
     COREPORT_BIT   (  0x0040, "Ticket Notch",     KEYCODE_K)  \
-	COREPORT_BIT   (  0x0080, "Dedicated Sw#20",  KEYCODE_L) \
+    COREPORT_BIT   (  0x0080, "Dedicated Sw#20",  KEYCODE_L) \
     COREPORT_BIT   (  0x0100, "Back",             KEYCODE_7) \
     COREPORT_BIT   (  0x0200, "Minus",            KEYCODE_8) \
     COREPORT_BIT   (  0x0400, "Plus",             KEYCODE_9) \
     COREPORT_BIT   (  0x0800, "Select",           KEYCODE_0) \
-	/*Switch Col. 2*/ \
+    /*Switch Col. 2*/ \
     COREPORT_BIT   (  0x8000, "Start Button",     KEYCODE_1) \
     COREPORT_BIT   (  0x4000, "Tournament Start", KEYCODE_2) \
     /*Switch Col. 9*/ \
@@ -139,7 +124,7 @@ FILE *fpSND = NULL;
     COREPORT_BITDEF(  0x0002, IPT_COIN2,          KEYCODE_4)  \
     COREPORT_BITDEF(  0x0004, IPT_COIN3,          KEYCODE_5)  \
     COREPORT_BITDEF(  0x0008, IPT_COIN4,          KEYCODE_6)  \
-	/*None*/ \
+    /*None*/ \
     COREPORT_BITTOG(  0x1000, "Coin Door",        KEYCODE_END) \
   PORT_START /* 1 */ \
     COREPORT_DIPNAME( 0x001f, 0x0000, "Country") \
@@ -202,46 +187,39 @@ const struct sndbrdIntf spaIntf = {
 	"SPA", spasnd_init, NULL, NULL, man3_w, scmd_w, NULL, NULL, NULL, SNDBRD_NODATASYNC
 };
 
-int spa_sndbufferlength()
+static int spa_sndbufferlength()
 {
 	if (spalocals.sampnum >= spalocals.sampout)
 		return spalocals.sampnum - spalocals.sampout;
 	else
 		return spalocals.sampnum + SPA_SNDBUFSIZE - spalocals.sampout;
-
 }
 
-
-static void spa_sh_update(int num, INT16 *buffer[2], int length)
+static void spa_sh_update(int num, INT16 *buffer, int length)
 {
 	if (length > 0)
 	{
-		int ii = 0, channel;
-
+		int ii;
 		for (ii = 0; ii < length && spalocals.sampout != spalocals.sampnum; ii++)
 		{
-			for (channel = 0; channel < 2; channel++)
-			{
-				spalocals.lastsamp[channel] = buffer[channel][ii] = spalocals.samplebuf[channel][spalocals.sampout];
-			}
+			spalocals.lastsamp = buffer[ii] = spalocals.samplebuf[spalocals.sampout];
+
 			if (++spalocals.sampout == SPA_SNDBUFSIZE)
 				spalocals.sampout = 0;
 		}
 
 		for (; ii < length; ++ii)
-			for (channel = 0; channel < 2; channel++)
-				buffer[channel][ii] = spalocals.lastsamp[channel];
+			buffer[ii] = spalocals.lastsamp;
 	}
 }
 
 static int spa_sh_start(const struct MachineSound *msound)
 {
-	const char *stream_name[] = { "SPA Left", "SPA Right" };
-	const int volume[2] = { MIXER(100,MIXER_PAN_LEFT), MIXER(100,MIXER_PAN_RIGHT) };
+	const char *stream_name = "SPA";
+	const int volume = 100;
 	/*-- allocate a DAC stream at fixed frequency --*/
-	return stream_init_multi
-	(2,
-		stream_name,
+	return stream_init
+	(	stream_name,
 		volume,
 		SPA_SOUNDFREQ,
 		0,
@@ -267,28 +245,27 @@ struct nvram
 struct nvram nv;
 
 
-void reset_nvram()
+static void reset_nvram()
 {
 	memset(&nv, 0, sizeof(struct nvram));
 	nv.volume = 80;
 	nv.freeplay = 1;
 }
 
-void critical_error(char *msg)
+static void critical_error(char *msg)
 {
 	MessageBox(NULL, msg, NULL, MB_OK | MB_ICONERROR);
 	exit(1);
 }
 
-char spa_dllpath[MAX_PATH];
-uint32_t dllcrc = 0;
-uint32_t imagecrc = 0;
+static uint32_t dllcrc = 0;
+static uint32_t imagecrc = 0;
 
 
 // Some very messy references to memory locations within TPA release build DLL, mostly found
 // with CheatEngine.
 
-int hsptr[] = { 0x990, 0x9b8, 0xaf0, 0xb40, 0xb90, 0xa70 };
+static const int hsptr[] = { 0x990, 0x9b8, 0xaf0, 0xb40, 0xb90, 0xa70 };
 #define knockptr (persistbuf - 0x32000 + hsptr[0] - 0xE4c + 0x28)
 #define langptr (knockptr - 0xa0)
 #define ballspergameptr (knockptr - 0x30)
@@ -310,29 +287,29 @@ static MACHINE_INIT(spa) {
 
 	if (once)
 	{
-		once = 0;
 		// Load the DLL
 		char path[MAX_PATH];
 
 #if (defined(VPINMAME) && !defined(_DEBUG))
 		HINSTANCE hInst;
 #ifndef _WIN64
-		hInst = GetModuleHandle("VPinMAME.dll");
+		hInst = GetModuleHandle("VPinSPA.dll");
 #else
-		hInst = GetModuleHandle("VPinMAME64.dll");  // if ever..
+		hInst = GetModuleHandle("VPinSPA64.dll");  // if ever..
 #endif
-		GetModuleFileName(hInst, spa_dllpath, 1024);
+		GetModuleFileName(hInst, spa_dllpath, MAX_PATH);
 		*strrchr(spa_dllpath, '\\') = 0;
 #else
-		strcpy(spa_dllpath, "C:\\Games\\Visual Pinball\\VPinMAME");
+		strcpy(spa_dllpath, "C:\\Visual Pinball\\VPinSPA");
 #endif
 
-		sprintf(path, "%s\\spagb100\\SternGB.dll", spa_dllpath);
+		once = 0;
 
+		sprintf_s(path, sizeof(path), "%s\\spagb100\\SternGB.dll", spa_dllpath);
 		FILE* f = fopen(path, "rb");
 		if (f == NULL) {
 			char tmp[MAX_PATH];
-			sprintf(tmp, "Failed to open %s", path);
+			sprintf_s(tmp, sizeof(tmp), "Failed to open %s", path);
 			critical_error(tmp);
 			return;
 		}
@@ -353,10 +330,12 @@ static MACHINE_INIT(spa) {
 		}
 
 		// Load the ROM
-		sprintf(path, "%s\\spagb100\\image.bin", spa_dllpath);
+		sprintf_s(path, sizeof(path), "%s\\spagb100\\image.bin", spa_dllpath);
 		f = fopen(path, "rb");
 		if (f == NULL) {
-			critical_error("Cannot open image.bin");
+			char tmp[MAX_PATH];
+			sprintf_s(tmp, sizeof(tmp), "Failed to open %s", path);
+			critical_error(tmp);
 			return;
 		}
 		fseek(f, 0, SEEK_END);
@@ -398,13 +377,12 @@ static MACHINE_INIT(spa) {
 		stern_get_lcd_frame_fn = (int(*)(void))GetProcAddress(dll, "stern_get_lcd_frame_fn");
 
 		// It turns out, the very first thing stern_step_fn does is force the profanity mode off.  Fuck that.  :)
-		// I thought it would require a DLL patch until I realized I can simply enter the step function later.  
+		// I thought it would require a DLL patch until I realized I can simply enter the step function later.
 		if (dllcrc == DLLCRC_TPAGBDLL)
 			stern_step_fn = (uint8_t *)stern_step_fn + 0x13;
 
-		if (stern_set_rom_data_fn == NULL) {
+		if (stern_set_rom_data_fn == NULL)
 			critical_error("Cannot find stern_set_rom_data in DLL.");
-		}
 
 		// Setup image.bin.
 		stern_set_rom_data_fn(data, size);
@@ -427,7 +405,7 @@ static MACHINE_RESET(spa) {
 	if (dllcrc == DLLCRC_TPAGBDLL)
 	{
 		// Turn the knocker on
-		int *mem = knockptr;
+		int *mem = (int*)knockptr;
 		(*mem) = 2;
 		(*(mem+1)) = 253;
 	}
@@ -461,25 +439,25 @@ static MACHINE_RESET(spa) {
 					}
 					else if (_strnicmp(p, "language", 7) == 0 && dllcrc == DLLCRC_TPAGBDLL)
 					{
-						int *mem = langptr;
+						int *mem = (int*)langptr;
 						(*mem) = atoi(p2);
 						(*(mem+1)) = 255-atoi(p2);
 					}
 					else if (_strnicmp(p, "profanity", 9) == 0 && (dllcrc == DLLCRC_TPAGBDLL))
 					{
-						int *mem = profanityptr;
+						int *mem = (int*)profanityptr;
 						(*mem) = atoi(p2);
 						(*(mem + 1)) = 255 - atoi(p2);
 					}
 					else if (_strnicmp(p, "scare", 9) == 0 && (dllcrc == DLLCRC_TPAGBDLL))
 					{
-						int *mem = scareptr;
+						int *mem = (int*)scareptr;
 						(*mem) = atoi(p2);
 						(*(mem + 1)) = 255 - atoi(p2);
 					}
 					else if (_strnicmp(p, "ballspergame", 12) == 0 && dllcrc == DLLCRC_TPAGBDLL)
 					{
-						int *mem = ballspergameptr;
+						int *mem = (int*)ballspergameptr;
 						(*mem) = atoi(p2);
 						(*(mem + 1)) = 255 - atoi(p2);
 					}
@@ -528,7 +506,7 @@ static MACHINE_STOP(spa) {
 	time_to_quit = 1;
 
 	char path[MAX_PATH];
-	sprintf(path, "%s\\spagb100\\gbnv.txt", spa_dllpath);
+	sprintf_s(path, sizeof(path), "%s\\spagb100\\gbnv.txt", spa_dllpath);
 	FILE *f = fopen(path, "w");
 	if (f)
 	{
@@ -547,13 +525,13 @@ static MACHINE_STOP(spa) {
 		stern_get_grand_champion_fn(&inits, &score);
 		if (dllcrc == DLLCRC_TPAGBDLL)
 		{
-			int *mem = langptr;
+			int *mem = (int*)langptr;
 			fprintf(f, "Language:%d\n", *mem);
-			mem = ballspergameptr;
+			mem = (int*)ballspergameptr;
 			fprintf(f, "BallsPerGame:%d\n", *mem);
-			mem = profanityptr;
+			mem = (int*)profanityptr;
 			fprintf(f, "Profanity:%d\n", *mem);
-			mem = scareptr;
+			mem = (int*)scareptr;
 			fprintf(f, "Scare:%d\n", *mem);
 		}
 		fprintf(f, "HiScore0:%s,%llu\n", inits, score);
@@ -596,7 +574,6 @@ static SWITCH_UPDATE(spa) {
 		// 0x1000(coin door) >> 12
 		/*Coin Door Switch - Not mapped to the switch matrix*/
 		spalocals.coindoor = (inports[SPA_COMINPORT] & 0x1000) ? 0 : 1;
-
 	}
 }
 
@@ -606,30 +583,29 @@ static int spa_getSol(int solNo)
 	return 0;
 }
 
+#ifdef REFRESH_HACK
 extern HWND win_video_window;
+#endif
 
 /********************/
 /*  VBLANK Section  */
 /********************/
-static INTERRUPT_GEN(spa_vblank)
+static /*INTERRUPT_GEN(spa_vblank)*/ void spa_vblank(int data)
 {
-	core_updateSw(1);
-
 	int i;
+
+	core_updateSw(1);
 
 	// Switches
 	int sw = coreGlobals.swMatrix[0];
-	for (i = 8
-		; i < 12; i++)
+	for (i = 8; i < 12; i++)
 		stern_set_dedicated_switch_state_fn(i, 0, (sw &  (1 << (i - 4))) ? 1 : 0);
 
 	for (i = 0; i < 8; i++)
 		stern_set_dedicated_switch_state_fn(i, 0, core_getDip(0) & (1 << i));
 
 	for (i = 1; i < 90; i++)
-	{
 		stern_set_switch_state_fn(i % 16, i / 16, core_getSw(i));
-	}
 
 	// Solenoids
 	coreGlobals.solenoids = 0;
@@ -656,11 +632,13 @@ static INTERRUPT_GEN(spa_vblank)
 		if ((*mem) == 0x03)
 			coreGlobals.solenoids2 |= 0x10;
 	}
+
 	static int half = 0;
 	if (half++ == 1)
 		half = 0;
 	else
 	{
+#ifdef REFRESH_HACK
 		// There is a super bizzare problem where 
 		// the SetWindowsPos windows message is simply ignored for a while
 		// So we need to kick VPM to do this again after a couple frames.
@@ -677,10 +655,9 @@ static INTERRUPT_GEN(spa_vblank)
 			spalocals.refresh_size++;
 			break;
 		}
+#endif
 		time_to_quit |= updatescreen();
 		memset(&coreGlobals.lampMatrix[0], 0, 42);
-
-		int dst;
 
 		if (dllcrc == DLLCRC_TPAGBDLL) // We can directly read raw light values.   Lets us see the library colors correctly and get full ranges.
 		{
@@ -692,14 +669,15 @@ static INTERRUPT_GEN(spa_vblank)
 		}
 		else
 		{
-
+			int dst;
 			for (i = 0, dst = 0; i < 150; i++)
 			{
 				uint8_t clr[40];
 				int clrlast = 0;
+				int z;
 
 				stern_get_lamp_color(i, 0, &clr);
-				for (int z = 0; z < 3; z++)
+				for (z = 0; z < 3; z++)
 				{
 					clr[z] += 0x40;  // pba subtracts this inexplicably.. put it back!
 					clr[z] *= 2;
@@ -801,6 +779,7 @@ static INTERRUPT_GEN(spa_vblank)
 	while (spa_sndbufferlength() < SPA_SNDBUFSIZE - 800)
 	{
 		int audio_samples = 0;
+		int i;
 
 		int16_t* audio = stern_get_sound_buffer(&audio_samples);
 
@@ -809,17 +788,16 @@ static INTERRUPT_GEN(spa_vblank)
 
 		if (dllcrc == DLLCRC_TPAGBDLL || dllcrc == DLLCRC_SPAGBDLL)
 			audio_samples = 800;
-		for (int i = 0; i < audio_samples; i++)
+		for (i = 0; i < audio_samples; i++)
 		{
-			spalocals.samplebuf[0][spalocals.sampnum] = audio[i];
-			spalocals.samplebuf[1][spalocals.sampnum] = audio[i];
+			spalocals.samplebuf[spalocals.sampnum] = audio[i]; //!! no stereo??
 			if (++spalocals.sampnum == SPA_SNDBUFSIZE)
 				spalocals.sampnum = 0;
 		}
 	}
 	// Goofy things happen if you set switches shortly after calling this, so sleep after to ensure a timing blip doesn't cause this to loop quickly. 
 	stern_step_fn();
-	Sleep(1);
+	Sleep(1); //!!
 }
 
 /*-----------------------------------------------
@@ -851,7 +829,6 @@ MDRV_CORE_INIT_RESET_STOP(spa, spa, spa)
 MDRV_DIPS(8)
 MDRV_NVRAM_HANDLER(spa)
 MDRV_SOUND_ADD(CUSTOM, spaCustInt)
-MDRV_SOUND_ATTRIBUTES(SOUND_SUPPORTS_STEREO)
 MACHINE_DRIVER_END
 
 
@@ -869,22 +846,27 @@ MACHINE_DRIVER_END
 That's 8 bits for each pixel, but they are distributed into 4 brightness
 bits (16 colors), and 4 translucency bits that perform the masking of the
 secondary or "background" page that will "shine through" if the mask bits
-of the foreground are set.    SPA DLL appears to give us this same data.   Current simple
-implementation just looks to see if transparent, if so uses background page.  */
+of the foreground are set.    SPA DLL appears to give us this same data. */
 
 static PINMAME_VIDEO_UPDATE(spadmd_update) {
 	int y;
-	tDMDDot dotCol;
-
 	for (y = 0; y < 32; y++)
 	{
-		memcpy(&dotCol[y + 1][0], &spa_fbuffer[y * 128], 128);
-		for (int x = 0; x < 128; x++)
-			if (dotCol[y + 1][x] > 15) // Transparency set
-				dotCol[y + 1][x] = spa_bbuffer[y * 128 + x];  
+		int offs = y * 128;
+		int x;
+		for (x = 0; x < 128; x++,offs++)
+		{
+			const UINT8 RAM1 = spa_fbuffer[offs];
+			const UINT8 RAM2 = spa_bbuffer[offs];
+			const UINT8 mix = RAM1 >> 4;
+			const UINT8 temp = (RAM2 & mix) | (RAM1 & (mix ^ 0xF)); //!! is this correct or is mix rather a multiplier/ratio/alphavalue??
+			if ((mix != 0xF) && (mix != 0x0)) //!! happens e.g. in POTC in extra ball explosion animation: RAM1 values triggering this: 223, 190, 175, 31, 25, 19, 17 with RAM2 being always 0. But is this just wrong game data (as its a converted animation)?!
+				LOG(("Special DMD Bitmask %01X", mix));
+			coreGlobals.dotCol[y + 1][x] = temp;
+		}
 	}
 
-	video_update_core_dmd(bitmap, cliprect, dotCol, layout);
+	video_update_core_dmd(bitmap, cliprect, layout);
 	return 0;
 }
 
@@ -916,10 +898,8 @@ ROM_END
 /*-------------------------------------------------------------------
 / Ghostbusters LE
 /-------------------------------------------------------------------*/
-INITSPAGAME(spagb, GEN_SAM, spa_dmd128x32, SAM_8COL, SPA_GAME_GB)
+INITSPAGAME(spagb, GEN_SPA, spa_dmd128x32, SAM_8COL, SPA_GAME_GB)
 
-SPA_ROM32MB(spagb_100, "fg_1200ai.bin", CRC(078b0c9a) SHA1(f1472d2c4a06d674bf652dd481cce5d6ca125e0c), 0x01D9F8B8)
+SPA_ROM32MB(spagb_100, "XXX.bin", CRC(00000000) SHA1(0000000000000000000000000000000000000000), 0x00000000)
 SPA_INPUT_PORTS_START(spagb, 1)
-CORE_GAMEDEF(spagb, 100, "Ghostbusters LE", 2013, "Stern", spa, 0)
-
-
+CORE_GAMEDEF(spagb, 100, "Ghostbusters Limited Edition (V1.15)", 2013, "Stern", spa, 0)
